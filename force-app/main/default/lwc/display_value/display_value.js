9f85238bf20ed1d29095f7c84d2c01f6ead70ca7
Sachin210894 <sachinshah2108@gmail.com>
2024-02-01 17:28:57 +0530

    all changes

--- force-app/main/default/lwc/display_value/display_value.js
+++ force-app/main/default/lwc/display_value/display_value.js
@@ -1,39 +1,51 @@
 import { LightningElement, track } from "lwc";
+
 export default class Display_value extends LightningElement {
   @track selectedFormType = "";
-  @track name;
-  labelValue = "";
-  fieldValue = "";
+  @track labelValue = "";
+  @track fieldValue = "";
+  @track error = "";
 
   formTypeOptions = [
     { label: "Select Form Type", value: "" },
-    { label: "Text", value: "text" },
-    { label: "Number", value: "number" },
-    { label: "Auto-Number", value: "autonumber" },
-    { label: "phone", value: "phone" },
-    { label: "URL", value: "url" }
+    { label: "Text", value: "text", regex: /^[\s\S]*$/ },
+    { label: "Number", value: "number", regex: /^\d+$/ },
+    { label: "Auto-Number", value: "autonumber", regex: /^\d+$/ },
+    { label: "Phone", value: "phone", regex: /^\+?\d+$/ },
+    { label: "URL", value: "url", regex: /^(ftp|http|https):\/\/[^ "]+$/ },
+    { label: "Email", value: "email", regex: /^[^\s@]+@[^\s@]+\.[^\s@]+$/ }
   ];
 
   handleFormTypeChange(event) {
     this.selectedFormType = event.detail.value;
-    // console.log('Value is:',this.selectedFormType);
+    this.resetError();
   }
 
-  displayfield(event) {
+  handleFieldChange(event) {
     this.fieldValue = event.target.value;
     this.labelValue = this.fieldValue + "__c";
-    // this.name= event.target.value;
-    //  console.log('Name is:',this.name);
+    this.resetError();
   }
 
-  // displaylabel(event) {
-  //     this.label= event.target.value +'__c' ;
-  //   //   console.log('Name1 is:',this.label);
-  // }
+  resetError() {
+    this.error = "";
+  }
 
   handleSubmit() {
+    const formType = this.formTypeOptions.find(
+      (option) => option.value === this.selectedFormType
+    );
+
+    if (formType && formType.regex) {
+      if (!formType.regex.test(this.fieldValue)) {
+        this.error = `Please enter a valid ${formType.label} value.`;
+        return;
+      }
+    }
+
     console.log("Selected Form Type:", this.selectedFormType);
     console.log("Field Name:", this.fieldValue);
     console.log("Label:", this.labelValue);
+    this.resetError();
   }
 }
