64d7b3b63ccaa9d5be381fbff18e3df103829ee1
Sachin210894 <sachinshah2108@gmail.com>
2024-02-05 17:49:41 +0530

    all changes2

---
+++ force-app/main/default/lwc/a2z_banner/a2z_banner.js
@@ -0,0 +1,5 @@
+import { LightningElement } from "lwc";
+import BANNER from "@salesforce/resourceUrl/Electronc_gadget";
+export default class A2z_banner extends LightningElement {
+  myBanner = BANNER;
+}
---
+++ force-app/main/default/lwc/a2z_home/a2z_home.js
@@ -0,0 +1,9 @@
+import { LightningElement } from "lwc";
+import BANNER from "@salesforce/resourceUrl/banner1";
+import BANNER2 from "@salesforce/resourceUrl/banner2";
+import BANNER3 from "@salesforce/resourceUrl/banner3";
+export default class A2z_home extends LightningElement {
+  myBanner = BANNER;
+  myBanner2 = BANNER2;
+  myBanner3 = BANNER3;
+}
