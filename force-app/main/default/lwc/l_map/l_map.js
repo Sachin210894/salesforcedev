97c2e91e765e577d07d1fcd5b76057709ce5d49e
Sachin210894 <sachinshah2108@gmail.com>
2024-02-19 12:09:55 +0530

    map changes

---
+++ force-app/main/default/lwc/l_map/l_map.js
@@ -0,0 +1,32 @@
+import { LightningElement, wire, track } from "lwc";
+import fetchAccounts from "@salesforce/apex/getmapaccount.fetchAccounts";
+
+export default class L_map extends LightningElement {
+  @track error;
+  @track mapMarkers = [];
+  @track markersTitle = "Accounts";
+  @track zoomLevel = 4;
+
+  /* Load address from Controller */
+  @wire(fetchAccounts, {})
+  wireAccount({ error, data }) {
+    if (data) {
+      data.forEach((dataItem) => {
+        this.mapMarkers = [
+          ...this.mapMarkers,
+          {
+            location: {
+              City: dataItem.BillingCity,
+              Country: dataItem.BillingCountry
+            },
+            icon: "custom:custom26",
+            title: dataItem.Name
+          }
+        ];
+      });
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+    }
+  }
+}
